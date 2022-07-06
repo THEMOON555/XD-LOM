54fc3f928e05c6d537f43b86ca491c5f4443ccb2
TheDemon <108716811+THEMOON555@users.noreply.github.com>
2022-07-06 12:46:41 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,73 @@
+
+const fetch = require('node-fetch');
+const moment = require('moment');
+const chalk = require('chalk');
+const rs = require('readline-sync');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const GoStumble = (auth) => new Promise((resolve, reject) => {
+
+  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+    method: 'GET',
+    headers: {
+      'authorization': auth
+    }
+  })
+    .then(res => res.text())
+    .then(data => {
+      resolve(data);
+    })
+    .catch(err => {
+      reject(err);
+    });
+
+});
+
+(async () => {
+
+  console.log(` ▄▄▄    █▄█▒▒██   ██▒
+▄    ▓██ ▒ ██▒     ▄    ▒ ▒ ▒ ▄▀█ ▒░
+▒██ ░▄█▓██  ▀█▒▒█ ▄▀▒░ ░
+░█ ▒██  ▓██▒  ██▄█   ▄▀▒   ░ ░ █ █ ▒ 
+ ▓█   ▓██▒░██▓ ▒█▓██░ ▓█   ▓█▒▒█ ▒██▒
+ ▒▒   ▓▒█░░ ▒▓ ░▒▓░░ ▒  ▒▒   ░░▓░▓ ░
+  ▒   ▒▒ ░  ░ ▒░░ ░░   ░ ▒░  ▒l░░   ░
+  ░   ▒     ░░   ░    ░   ░ ░    ░ ░ ░░      
+      ░  ░                 ░         ░       
+                                                     
+By : ${chalk.bold('DEMONXzx')}
+`);
+
+  const auth = rs.question(' Code Auth : ');
+  console.log('');
+
+  while (true) {
+
+    const result = await GoStumble(auth);
+    if (!result) {
+
+      console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] Authentication Code Not Valid`));
+      break;
+
+    } else if (result.includes('User')) {
+
+      const data = JSON.parse(result);
+      const username = data.User.Username;
+      const country = data.User.Country;
+      const trophy = data.User.SkillRating;
+      const crown = data.User.Crowns;
+
+console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.red(`User By DemonsX: ${username}`)} | ${chalk.blue(`Trophy By Demonz : ${trophy}`)} | ${chalk.green(`Crown By DemonZX: ${crown}`)}`));
+      await sleep(6000);
+
+    } else if (result == 'BANNED') {
+      console.log(chalk.bgRed(`Mampus ke banned akunnya wkwkwkwk `));
+     break;
+    }
+  }
+
+
+})();
